dd5237616fe86dacbeb9df90479ab1d265b95b4f
Hassan Shaikley <hassan.shaikley@gmail.com>
2025-09-16 11:35:31 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import "./index.css";
+import { APITester } from "./APITester";
+
+import logo from "./logo.svg";
+import reactLogo from "./react.svg";
+
+export function App() {
+  return (
+    <div className="max-w-7xl mx-auto p-8 relative z-10 flex flex-row">
+      <div className="w-48">
+        <ul>
+          <LeftItem text="Essays" />
+        </ul>
+      </div>
+
+      <div>
+        <h1 className="text-4xl font-bold my-4 leading-tight">
+          Hassan Shaikley
+        </h1>
+        <p>What's up doc</p>
+        <APITester />
+      </div>
+    </div>
+  );
+}
+
+const LeftItem = (props) => {
+  return <li>{props.text}</li>;
+};
+
+export default App;
